8a86b904199f72e2c0ed32d4a62c5597ae2f557c
tatanasoboleva <helllleh5@yandex.ru>
2018-11-12 17:12:38 +0400

    v1

---
+++ src/model/FootballСlub.java
@@ -0,0 +1,12 @@
+package model;
+
+import java.util.ArrayList;
+
+public class FootballСlub {
+    private String name;
+    private String league;
+    private double budget;
+    private Сountry country;
+    private ArrayList<Player> players;
+
+}
---
+++ src/model/Player.java
@@ -0,0 +1,12 @@
+package model;
+
+public class Player {
+    private String firstName;
+    private String secondName;
+    private String nationality;
+    private int age;
+    private FootballСlub footballСlub;
+    private int position;
+    private double cost;
+
+}
